422ed7bb7a5a14e43bcd06ebbbd0187e576ea20b
Daniel Naylor <git@drnaylor.co.uk>
2014-01-26 15:07:26 +0000

    Patch to JsonUptimeConverter where the uptime is null, rather than a time.

--- API/JsonConverters/JsonUptimeConverter.cs
+++ API/JsonConverters/JsonUptimeConverter.cs
@@ -30,13 +30,13 @@ namespace McMyAdminAPI.JsonConverters
         /// <param name="objectType">The <see cref="Type"/> of object to create.</param>
         /// <param name="existingValue">The object that currently exists.</param>
         /// <param name="serializer">The <see cref="JsonSerializer"/> to use.</param>
-        /// <returns>The deserialised object.</returns>
+        /// <returns>The deserialised object, or <code>null</code> if the Uptime does not exist.</returns>
         public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
         {
-            var jsonObject = JObject.Load(reader);
-
             try
             {
+                var jsonObject = JObject.Load(reader);
+
                 // Create the target object. We perform the serialization manually at this step.
                 return new TimeSpan(
                     JsonConvert.DeserializeObject<int>(jsonObject["days"].ToString()),
@@ -46,12 +46,13 @@ namespace McMyAdminAPI.JsonConverters
             }
             catch (Exception)
             {
+                // This means we don't have a valid uptime. Return null.
                 return null;
             }
         }
 
         /// <summary>
-        /// Writes the Json to a Json string.
+        /// Writes the Json to a Json string. Throws a <see cref="NotImplementedException"/>.
         /// </summary>
         /// <param name="writer">The <see cref="JsonWriter"/> to use.</param>
         /// <param name="value">The object to serialise.</param>
